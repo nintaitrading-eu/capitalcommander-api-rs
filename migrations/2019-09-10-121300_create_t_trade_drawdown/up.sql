4e0a75ecc51967efd51a638656dd279e17a9b59b
Andy Nagels <thereisanewway@gmail.com>
2019-09-11 14:11:50 +0200

    Added migration for T_TRADE_DRAWDOWN.

--- migrations/2019-09-10-121300_create_t_trade_drawdown/up.sql
+++ migrations/2019-09-10-121300_create_t_trade_drawdown/up.sql
@@ -1,1 +1,13 @@
--- Your SQL goes here
+CREATE TABLE T_TRADE_DRAWDOWN
+(
+    trade_drawdown_id bigserial not null,
+    drawdown_current int not null,
+    drawdown_max int not null,
+    is_deleted boolean not null default 'false',
+    date_created timestamp with time zone not null,
+    date_modified timestamp with time zone not null default (now() at time zone 'UTC'),
+    constraint pk_trade_drawdown_id primary key(trade_drawdown_id),
+    unique(trade_drawdown_id),
+    check(extract(timezone from date_created) = '0'),
+    check(extract(timezone from date_modified) = '0')
+);
