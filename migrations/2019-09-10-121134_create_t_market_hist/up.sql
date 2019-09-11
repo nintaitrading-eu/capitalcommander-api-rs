c30b5571f50fb0959f292de148a140ee91e8553e
Andy Nagels <thereisanewway@gmail.com>
2019-09-11 10:46:17 +0200

    Added migration for T_MARKET_HIST.

--- migrations/2019-09-10-121134_create_t_market_hist/up.sql
+++ migrations/2019-09-10-121134_create_t_market_hist/up.sql
@@ -1,1 +1,20 @@
--- Your SQL goes here
+CREATE TABLE T_MARKET_HIST
+(
+    market_hist_id bigserial not null,
+    market_id bigint not null,
+    code varchar(50) not null,
+    name varchar(50) not null,
+    country char(2) not null,
+    is_deleted int not null,
+    date_created timestamp with time zone not null,
+    date_modified timestamp with time zone not null,
+    date_hist_created timestamp with time zone not null,
+    date_hist_modified timestamp with time zone not null default (now() at time zone 'UTC'),
+    constraint pk_market_hist_id primary key(market_hist_id),
+    constraint fk_T_MARKET_HIST_market_id foreign key (market_id) references T_MARKET (market_id),
+    check(extract(timezone from date_created) = '0'),
+    check(extract(timezone from date_modified) = '0'),
+    check(extract(timezone from date_hist_created) = '0'),
+    check(extract(timezone from date_hist_modified) = '0')
+);
+COMMENT ON TABLE T_MARKET_HIST IS 'History table for T_MARKET.';
