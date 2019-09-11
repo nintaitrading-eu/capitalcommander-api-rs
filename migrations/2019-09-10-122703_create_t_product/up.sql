efd5cbc713343eb5ec4100c5ebad521c4de4c8ab
Andy Nagels <thereisanewway@gmail.com>
2019-09-11 15:25:16 +0200

    Added migrations for T_PRODUCT.

--- migrations/2019-09-10-122703_create_t_product/up.sql
+++ migrations/2019-09-10-122703_create_t_product/up.sql
@@ -1,1 +1,23 @@
--- Your SQL goes here
+CREATE TABLE T_PRODUCT
+(
+    product_id bigserial not null,
+    name varchar(50) not null,
+    description varchar(4000) not null,
+    product_type_id bigint not null,
+    currency_id bigint not null,
+    market_id bigint not null,
+    product_tick_info_id bigint,
+    is_deleted boolean not null default 'false',
+    date_created timestamp with time zone not null,
+    date_modified timestamp with time zone not null default (now() at time zone 'UTC'),
+    constraint pk_product_id primary key(product_id),
+    unique(product_id),
+    constraint fk_T_PRODUCT_currency_id foreign key(currency_id) references T_CURRENCY(currency_id),
+    constraint fk_T_PRODUCT_market_id foreign key(market_id) references T_MARKET(market_id),
+    constraint fk_T_PRODUCT_product_type_id foreign key(product_type_id) references T_PRODUCT_TYPE(product_type_id),
+    constraint fk_T_PRODUCT_product_tick_info_id foreign key(product_tick_info_id) references T_PRODUCT_TICK_INFO(product_tick_info_id),
+    check(extract(timezone from date_created) = '0'),
+    check(extract(timezone from date_modified) = '0')
+);
+COMMENT ON TABLE T_PRODUCT IS 'Table with products. This is the detailed version of the type of contract or other product_type you deal with. E.g: CCZ3.cfd';
+COMMENT ON COLUMN T_PRODUCT.product_tick_info_id IS 'May be NULL! The link is only valid for products that are cfd or futures contracts.';
--- migrations/2019-09-10-122728_create_t_product_hist/up.sql
+++ migrations/2019-09-10-122728_create_t_product_hist/up.sql
@@ -1,1 +1,24 @@
--- Your SQL goes here
+CREATE TABLE T_PRODUCT_HIST
+(
+        product_hist_id bigserial not null,
+        product_id bigint not null,
+        name varchar(50) not null,
+        description varchar(4000) not null,
+        product_type_id bigint not null,
+        currency_id bigint not null,
+        market_id bigint not null,
+        product_tick_info_id bigint not null,
+        is_deleted boolean not null default 'false',
+        date_created timestamp with time zone not null,
+        date_modified timestamp with time zone not null,
+        date_hist_created timestamp with time zone not null,
+        date_hist_modified timestamp with time zone not null default (now() at time zone 'UTC'),
+        constraint pk_product_hist_id primary key(product_hist_id),
+        unique(product_hist_id),
+        constraint fk_T_PRODUCT_HIST_product_id foreign key (product_id) references T_PRODUCT (product_id),
+        check(extract(timezone from date_created) = '0'),
+        check(extract(timezone from date_modified) = '0'),
+        check(extract(timezone from date_hist_created) = '0'),
+        check(extract(timezone from date_hist_modified) = '0')
+);
+COMMENT ON TABLE T_PRODUCT_TYPE IS 'History table for T_PRODUCT.';
