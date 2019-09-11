3243b77459617585df906f7f1d35f292cf4e729b
Andy Nagels <thereisanewway@gmail.com>
2019-09-11 14:18:43 +0200

    Added migrations related to T_VERSION.

--- migrations/2019-09-10-121316_create_t_version/up.sql
+++ migrations/2019-09-10-121316_create_t_version/up.sql
@@ -1,1 +1,11 @@
--- Your SQL goes here
+CREATE TABLE T_VERSION
+(
+    version_id int not null,
+    database_version varchar(100) not null default '',
+    database_version_info varchar(100) not null default '',
+    application_version varchar(100) not null default '',
+    application_version_info varchar(100) not null default '',
+    date_created timestamp with time zone not null default '1900-01-01',
+    date_modified timestamp with time zone not null default '1900-01-01',
+    constraint pk_version_id primary key(version_id)
+);
--- migrations/2019-09-10-121327_insert_t_version/up.sql
+++ migrations/2019-09-10-121327_insert_t_version/up.sql
@@ -1,1 +1,1 @@
--- Your SQL goes here
+insert into t_version values(1, '0.0.1', 'First try!', '0.0.1', 'First try!', (select now() at time zone 'UTC'), (select now() at time zone 'UTC'));
